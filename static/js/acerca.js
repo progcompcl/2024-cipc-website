9592d028756f65aeb98e232189e8e3e3f871f834
bkorecic <bkorecic@users.noreply.github.com>
2024-04-26 11:49:44 +0000

    deploy: 85f794fa6de51d6c5673058d80e46ced2837c604

---
+++ static/js/acerca.js
@@ -0,0 +1,9 @@
+const carousel = JSCarousel({
+  carouselSelector: "#carousel-3",
+  slideSelector: ".slide",
+  enablePagination: false,
+  enableAutoplay: true,
+  autoplayInterval: 8000
+})
+
+carousel.create();
